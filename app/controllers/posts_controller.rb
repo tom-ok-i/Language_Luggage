c8ade6c4a8f26bc2d11cf1137a7114af14703ff5
tom-ok-i <ten21.t.tom@gmail.com>
2021-09-09 23:15:37 +0000

    [update]投稿編集機能と削除機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,8 +23,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post)
+  end
+
   def destroy
-    @post = post.find(params[:id])
+    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
