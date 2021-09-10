152b6a203596160e5bad962ffed5165ce594a5ee
tom-ok-i <ten21.t.tom@gmail.com>
2021-09-10 10:57:41 +0000

    [add]いいね機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   def favorited_by?(user)
-    favorites.where(user_id: user.id).exits?
+    favorites.where(user_id: user.id).exists?
   end
 
   has_one_attached :image
