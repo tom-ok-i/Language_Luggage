11d2b357aa708e2fa4d7da1d2651f509f737a156
tom-ok-i <ten21.t.tom@gmail.com>
2021-09-10 15:58:19 +0000

    [start]ユーザーインデックスと詳細画面と編集画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.page(params[:page]).per(10)
   end
 
   def show
     @user = User.find(params[:id])
+    @posts = Post.page(params[:page]).per(10).reverse_order
   end
 
   def edit
