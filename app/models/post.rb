687e26cc444a1cfbfb5b6fad50d63c920439f16d
tom-ok-i <ten21.t.tom@gmail.com>
2021-09-12 18:09:55 +0000

    [fix]他ユーザーの投稿がマイページに表示されていたため修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,15 @@ class Post < ApplicationRecord
 
   has_one_attached :image
 
+  validate :image_type
+
+private
+
+def image_type
+  if !image.blob.content_type.in?(%('image/jpeg image/png'))
+    image.purge # Rails6では、この1行は必要ない
+    errors.add(:image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
+  end
+end
+
 end
