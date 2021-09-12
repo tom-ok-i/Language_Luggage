14019b962753678e346b61d0d90b6de1020d084f
tom-ok-i <ten21.t.tom@gmail.com>
2021-09-12 20:25:23 +0000

    [finish]投稿部分テンプレートビューの修正完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,5 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   has_one_attached :image
-  validate :image_type
-
-private
-
-def image_type
-  if !image.blob.content_type.in?(%('image/jpeg image/png'))
-    image.purge # Rails6では、この1行は必要ない
-    errors.add(:image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
-  end
-end
 
 end
