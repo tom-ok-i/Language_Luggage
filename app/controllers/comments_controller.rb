20380d1fb47ca0a796138da30b79d523cda9d6cf
tom-ok-i <ten21.t.tom@gmail.com>
2021-09-18 01:04:19 +0000

    [update]コメント機能の非同期化実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.post_id = @post.id
+    @comment.user_id = current_user.id
+    @comment.save
   end
 
   def destroy
-    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.find(params[:id])
+    comment.destroy
   end
 
   private
