eb643bef71985a23bab708a9cd72cbe829856a0e
tom-ok-i <ten21.t.tom@gmail.com>
2021-09-23 07:11:06 +0000

    [fix]フォロー人数が非同期になっていなかったため修正、相互フォローしている場合のみチャットボタンが非同期で出現するように修正

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,18 +2,19 @@ class ChatsController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    rooms = current_user.user_room.pluck(:room_id)
-    user_room = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    rooms = current_user.user_rooms.pluck(:room_id)
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+
     unless user_rooms.nil?
-      @room = user_rooms.rooml
+      @room = user_rooms.room
     else
       @room = Room.new
       @room.save
-      UserRoom.create(user_id: current_user.id, room_id @room.id)
+      UserRoom.create(user_id: current_user.id, room_id: @room.id)
       UserRoom.create(user_id: @user.id, room_id: @room.id)
     end
     @chats = @room.chats
-    @chat = Chat/new(room_id: @room.id)
+    @chat = Chat.new(room_id: @room.id)
   end
 
   def create
@@ -23,8 +24,14 @@ class ChatsController < ApplicationController
   end
 
   private
-  def chat_patams
+  def chat_params
     params.require(:chat).permit(:message, :room_id)
   end
 
+  def following_each_other
+    user = User.find(params[:id])
+    unless current_user.following?(user) && user.following?(current_user)
+      redirect_to contents_path
+    end
+  end
 end
