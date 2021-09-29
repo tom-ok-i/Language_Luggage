137dd7d41f9ee4d081eae16dc3bfc7ad9f5bcec0
tom-ok-i <ten21.t.tom@gmail.com>
2021-09-29 06:38:50 +0000

    [fix]新規投稿、投稿編集、投稿削除後の遷移先をユーザーページに変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_to posts_path
+      redirect_to user_path(current_user)
     else
       render:new
     end
@@ -37,13 +37,13 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
     post.update(post_params)
-    redirect_to post_path(post)
+    redirect_to user_path(current_user)
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to user_path(current_user)
   end
 
   private
