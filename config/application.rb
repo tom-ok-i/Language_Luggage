fc93e42210d53c3785b0e90a96c51f6ae1af3b0c
tom-ok-i <ten21.t.tom@gmail.com>
2021-10-03 08:41:53 +0000

    [fix]autoload lib

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module LanguageLuggage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.autoload_paths += Dir.glob("#{config.root}/lib")
+    config.paths.add 'lib', eager_load: true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
